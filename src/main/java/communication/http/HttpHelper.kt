acf0e84837afaed33cf9f09e61febe30211836ca
kantasp <peter.kantas@idomsoft.hu>
2025-03-29 10:56:08 +0100

    Feature.: sslIgnoreHttpRequest

--- src/main/java/communication/http/HttpHelper.kt
+++ src/main/java/communication/http/HttpHelper.kt
@@ -9,20 +9,39 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 class HttpHelper {
-    var httpUtil = HttpUtil()
+    private val httpClient: HttpClient = makeSSLIgnoreHttpClient()
 
-    fun sendJsonRequestWithoutURLEncoding(url:String,requestString:String):String {
+    private fun makeSSLIgnoreHttpClient(): HttpClient {
+        return try {
+            val sslContext = SSLContext.getInstance("TLS")
+            sslContext.init(null, arrayOf<TrustManager>(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate?> = arrayOfNulls(0)
+            }), SecureRandom())
+
+            HttpClient.newBuilder().sslContext(sslContext).build()
+        } catch (e: Exception) {
+            throw RuntimeException("Error creating SSL-ignoring HTTP client", e)
+        }
+    }
+
+    fun sendJsonRequestWithoutURLEncoding(url: String, requestString: String): String {
         val request = HttpRequest.newBuilder()
             .uri(URI.create(url))
             .header("Content-Type", "application/json; charset=utf-8")
             .POST(HttpRequest.BodyPublishers.ofString(requestString))
             .build()
 
-        val client = HttpClient.newHttpClient()
-        val httpresponse = client.send(request, HttpResponse.BodyHandlers.ofString())
-        return httpresponse.body()
+        val httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString())
+        return httpResponse.body()
     }
 
     fun sendJSONRequest(
@@ -31,8 +50,7 @@ class HttpHelper {
         headerName: String,
         headerValue: String,
         requestType: RequestType
-    ): JsonNode {
-        val response: JsonNode
+    ): String {
         return try {
             println("Request --------------")
             if (requestType == RequestType.GET || requestType == RequestType.DELETE) {
@@ -41,24 +59,23 @@ class HttpHelper {
                 println(commonRequest)
             }
             println("Response --------------")
-            response = when (requestType) {
-                RequestType.POST -> httpUtil.sendJsonRequestPOST(
-                    apiUrl,
-                    commonRequest,
-                    headerName,
-                    headerValue,
-                    requestType
-                )
 
-                RequestType.GET -> httpUtil.sendJsonRequestGET(apiUrl)
-                RequestType.DELETE -> httpUtil.sendJsonRequestDELETE(apiUrl)
-                RequestType.PUT -> TODO()
-                RequestType.PATCH -> TODO()
+            val requestBuilder = HttpRequest.newBuilder().uri(URI.create(apiUrl))
+                .header(headerName, headerValue)
+                .header("Content-Type", "application/json")
+
+            val request = when (requestType) {
+                RequestType.POST -> requestBuilder.POST(HttpRequest.BodyPublishers.ofString(commonRequest)).build()
+                RequestType.GET -> requestBuilder.GET().build()
+                RequestType.DELETE -> requestBuilder.DELETE().build()
+                RequestType.PUT, RequestType.PATCH -> throw UnsupportedOperationException("Method not implemented")
             }
+
+            val response = httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body()
             println(response)
             response
-        } catch (e: java.lang.Exception) {
-            throw java.lang.RuntimeException(e)
+        } catch (e: Exception) {
+            throw RuntimeException("HTTP request failed", e)
         }
     }
 
@@ -71,8 +88,8 @@ class HttpHelper {
                 val response = sendXMLRequest(requestBody, requestURL, headers, RequestType.POST)
                 CommonUtils.printDocument(response)
                 response
-            } catch (e: java.lang.Exception) {
-                throw java.lang.RuntimeException(e)
+            } catch (e: Exception) {
+                throw RuntimeException("XML request failed", e)
             }
         }
 
@@ -84,8 +101,8 @@ class HttpHelper {
                 val response = sendXMLRequest(requestBody, requestURL, headers, RequestType.GET)
                 CommonUtils.printDocument(response)
                 response
-            } catch (e: java.lang.Exception) {
-                throw java.lang.RuntimeException(e)
+            } catch (e: Exception) {
+                throw RuntimeException("XML GET request failed", e)
             }
         }
     }
